ebaba675315dd8c5913b556ba293728ce327245f
MkeniniZaatout <mkeninismael2@gmail.com>
2021-05-23 17:46:34 +0200

    connexion with mongodb

--- site_serveur/server.js
+++ site_serveur/server.js
@@ -1,12 +1,38 @@
-var express = require('express');
-var server = express();
-var morgan = require("morgan");
-var router = require("./router");
+const express = require('express');
+const server = express();
+const morgan = require("morgan");
+const router = require("./router");
+const mongoose = require('mongoose');
 
-server.use(express.static('public'));
+mongoose.connect("mongodb://localhost/biblio", {useNewUrlParser:true, useUnifiedTopology:true});
+/**
+ * CONNEXION :
+*/
+
+const livreSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    nom: String,
+    auteur: String,
+    annee: Number, 
+    pages: Number 
+});
+
+const livreModel = mongoose.model("livre", livreSchema);
+
+const allLivre = 
+    livreModel.find()
+        .exec()
+        .then(livres => {
+            console.log("livres",livres);
+        })
+        .catch();
+
 // NB : Utiliser les modules morgan et nodemon seulement en dev
 // pour l'affichage des logs
 server.use(morgan('dev'));
+server.use(express.static('public'));
+
+
 server.use("/", router);
 server.use("/livres", router);
 server.listen(3000);
