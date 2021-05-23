b0e3700bb59db77590ba8c87caea5d2acd909e30
MkeniniZaatout <mkeninismael2@gmail.com>
2021-05-23 17:08:36 +0200

    add livrer.html.twig

--- site_serveur/router.js
+++ site_serveur/router.js
@@ -3,13 +3,18 @@ var routeur = express.Router();
 const twig = require('twig');
 
 routeur.get('/', (req, res) => {
-    res.render("home.html.twig");
+    res.render("home.html.twig");    
 });
          
 routeur.get('/livres', (req, res) => {
     console.log(req.path);
-    res.render("livres/liste.html.twig");
-});     
+    res.render("livres/liste.html.twig");  
+});
+
+routeur.get("/livres/:nom", (req,res) => {
+    console.log(req.params.nom);
+    res.render("livres/livre.html.twig",{nom: req.params.nom}); 
+});
 
 routeur.use((req,res, next)=> {
     const erreur = new Error('Error page not found');
